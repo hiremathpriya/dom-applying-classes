f5c5a5dea025eed8fd38103893653ab4fc8ee1d6
Priya Hiremath <priyahrish18@gmail.com>
2017-11-10 21:53:25 +1300

    dom applying classes

--- classes.js
+++ classes.js
@@ -1,12 +1,14 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+   one()
+   two()
+   three()
+   makeVisible()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -17,7 +19,18 @@ function one () {
   // Next, we apply a new CSS class to it:
   one.classList.add('blue')
 }
-
+function two(){
+  var two = document.getElementById('two')
+  two.classList.add('green')
+}
+function three() {
+  var three = document.getElementById('three')
+  three.classList.add('red')
+}
+function makeVisible() {
+  var makeVisible = document.getElementsByClassName('invsible')
+  makeVisible.classList.add('visible')
+}
 // CREATE FUNCTION two HERE
 
 // CREATE FUNCTION three HERE
